cbf8d737ce9906ec6371b8b215b37bc0017492f9
Mark Glen Martin <martinmarkglen80@gmail.com>
2026-02-09 15:29:11 +0800

    Serve root folder for image, no subfolder needed

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-// Serve static files
-app.use('/css', express.static(path.join(__dirname, 'css')));
-app.use('/images', express.static(path.join(__dirname, 'images')));
+// Parse form data
+app.use(express.urlencoded({ extended: true }));
+
+// Serve the root folder for images
+app.use(express.static(__dirname));
 
 // Routes
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'login.html')));
 
-// Demo login POST
+// Simple demo login
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
   if (username && password) {
@@ -21,4 +22,6 @@ app.post('/login', (req, res) => {
   }
 });
 
+// Start server
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Vaporia app running on port ${PORT}`));
