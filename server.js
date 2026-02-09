078d3d019cb33b1571173209214c91914e16d275
Mark Glen Martin <martinmarkglen80@gmail.com>
2026-02-09 14:34:49 +0800

    Initial commit of Vaporia project

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Redirect root to index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
