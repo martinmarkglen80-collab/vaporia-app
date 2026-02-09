026da5a6728f153a4ccc8c75dd55491fb4feae60
Mark Glen Martin <martinmarkglen80@gmail.com>
2026-02-09 14:58:14 +0800

    Ready for Railway deployment

--- server.js
+++ server.js
@@ -3,34 +3,22 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware to parse form data
-app.use(express.urlencoded({ extended: true }));
-
-// Serve static files (CSS, images, JS)
+// Serve static files
 app.use('/css', express.static(path.join(__dirname, 'css')));
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 // Routes
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'login.html')));
 
-app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, 'login.html'));
-});
-
-// Simple login POST (demo only)
+// Demo login POST
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
-
   if (username && password) {
-    res.send(`<h1>Welcome, ${username}!</h1><p><a href="/">Back to Home</a></p>`);
+    res.send(`<h1>Welcome, ${username}!</h1><p><a href="/">Home</a></p>`);
   } else {
     res.send(`<h1>Login failed</h1><p><a href="/login">Try again</a></p>`);
   }
 });
 
-// Start server
-app.listen(PORT, () => {
-  console.log(`Vaporia app running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Vaporia app running on port ${PORT}`));
